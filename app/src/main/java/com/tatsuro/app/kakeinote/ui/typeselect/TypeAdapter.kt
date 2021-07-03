a09781af13332cd6fd279e2e90d238ccf00ea4d3
Tatsuro <tatsuro.secure.mail@protonmail.ch>
2021-07-03 16:33:01 +0900

    #18の対応

--- app/src/main/java/com/tatsuro/app/kakeinote/ui/typeselect/TypeAdapter.kt
+++ app/src/main/java/com/tatsuro/app/kakeinote/ui/typeselect/TypeAdapter.kt
@@ -1,43 +1,75 @@
 package com.tatsuro.app.kakeinote.ui.typeselect
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.button.MaterialButton
-import com.tatsuro.app.kakeinote.R
 import com.tatsuro.app.kakeinote.constant.IncomeOrExpenseType
+import com.tatsuro.app.kakeinote.databinding.TypeItemBinding
 
-// TODO:findViewByIdからViewBindingに書き換える。
+/**
+ * 種類のアダプタ
+ *
+ * [TypeSelectBottomSheet]にて選択対象の収支種類を[RecyclerView]に紐付ける。
+ * @property types 選択対象の収支種類
+ * @property listener 種類選択ボタンのクリックリスナ
+ */
 class TypeAdapter(
     private val types: Array<IncomeOrExpenseType>,
     private val listener: OnItemClickListener
 ) : RecyclerView.Adapter<TypeAdapter.ViewHolder>() {
 
+    /** 種類選択ボタンがクリックされたときに呼び出されるコールバックのためのインターフェース定義 */
     fun interface OnItemClickListener {
-        fun onItemClick(item: IncomeOrExpenseType)
+
+        /**
+         * 種類選択ボタンがクリックされたときに呼び出される。
+         * @param type クリックされた収支種類
+         */
+        fun onItemClick(type: IncomeOrExpenseType)
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val textButton: MaterialButton = view.findViewById(R.id.textButton)
-    }
+    /**
+     * 種類選択ボタンのビューホルダ
+     * @property binding 種類項目のビューバインディング
+     * @property listener 種類選択ボタンのクリックリスナ
+     */
+    class ViewHolder(
+        private val binding: TypeItemBinding,
+        private val listener: OnItemClickListener
+    ) : RecyclerView.ViewHolder(binding.root) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater
-            .from(parent.context)
-            .inflate(R.layout.type_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textButton.apply {
-            setIconResource(types[position].drawableResId)
-            setText(types[position].strResId)
-            setOnClickListener {
-                listener.onItemClick(types[position])
+        /**
+         * ビューバインド
+         *
+         * 種類選択ボタンと引数[type]をバインドする。
+         * @param type 収支の種類
+         */
+        fun bind(type: IncomeOrExpenseType) {
+            binding.textButton.apply {
+                setIconResource(type.drawableResId)
+                setText(type.strResId)
+                setOnClickListener {
+                    listener.onItemClick(type)
+                }
             }
         }
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = TypeItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+        return ViewHolder(binding, listener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(types[position])
+    }
+
+    /**
+     * 選択対象の収支種類の数を返す。
+     * @return 選択対象の収支種類の数
+     */
     override fun getItemCount() = types.size
 }
