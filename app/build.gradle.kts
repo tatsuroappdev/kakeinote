1d27ec81af7c1c9be26c3ad95e44869e73792de3
Tatsuro <tatsuro.secure.mail@protonmail.ch>
2021-07-23 14:05:08 +0900

    dependenciesの変更
    - バージョンアップする。
    - 使用予定のライブラリを追加する。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,8 +76,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1-native-mt")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.1-native-mt")
 
     // AndroidX
     implementation("androidx.core:core-ktx:1.6.0")
@@ -86,6 +86,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.4.0-alpha04")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("androidx.preference:preference:1.1.1")
 
     // AndroidX Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
@@ -104,15 +105,19 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
 
     // Flipper
-    debugImplementation("com.facebook.flipper:flipper:0.98.0")
+    debugImplementation("com.facebook.flipper:flipper:0.99.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.98.0")
-    releaseImplementation("com.facebook.flipper:flipper-noop:0.98.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.99.0")
+    releaseImplementation("com.facebook.flipper:flipper-noop:0.99.0")
 
     implementation("com.orhanobut:logger:2.2.0")
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testImplementation("org.mockito:mockito-core:3.11.2")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
