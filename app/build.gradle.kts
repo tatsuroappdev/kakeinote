8e1d9884c402543b985940177f74e6268571fa9f
Tatsuro <tatsuro.secure.mail@protonmail.ch>
2021-10-19 00:01:23 +0900

    バージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ dependencies {
     // AndroidX
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
     implementation("androidx.fragment:fragment-ktx:1.4.0-alpha08")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
@@ -114,18 +114,18 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
 
     // Flipper
-    debugImplementation("com.facebook.flipper:flipper:0.105.0")
+    debugImplementation("com.facebook.flipper:flipper:0.115.0")
     debugImplementation("com.facebook.soloader:soloader:0.10.1")
-    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.105.0")
-    releaseImplementation("com.facebook.flipper:flipper-noop:0.105.0")
+    debugImplementation("com.facebook.flipper:flipper-leakcanary2-plugin:0.115.0")
+    releaseImplementation("com.facebook.flipper:flipper-noop:0.115.0")
 
     implementation("com.orhanobut:logger:2.2.0")
     debugImplementation("com.squareup.leakcanary:leakcanary-android:2.7")
 
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.assertj:assertj-core:3.20.2")
-    testImplementation("org.mockito:mockito-core:3.12.4")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+    testImplementation("org.assertj:assertj-core:3.21.0")
+    testImplementation("org.mockito:mockito-core:4.0.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
